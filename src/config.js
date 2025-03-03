fe9d22ccd5e77b07cc3542644472d95219e6e48b
JamesAri <James.Ari@outlook.com>
2025-03-03 13:08:52 +0100

    reflecting lib changes

--- src/config.js
+++ src/config.js
@@ -3,10 +3,8 @@ const { logLevels } = require('@slechtaj/service-client')
 const { getPublicInterface } = require('./utils')
 
 module.exports = {
-	connection: {
-		host: getPublicInterface(),
-		port: process.env.PORT || 0, // 0 means random port
-	},
+	host: getPublicInterface(),
+	port: process.env.PORT || 0, // 0 means random port
 	zk: 'zk://localhost:2181/hera-test',
 	logLevel: logLevels.ALL,
 }
